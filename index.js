270b4adac838c37f0fadcdd82a85ab9f5feb1d82
thadeusdev <109853680+thadeusdev@users.noreply.github.com>
2022-11-21 16:52:31 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+function expression(number, operation){
+  if(!operation){
+    return number
+  }else{
+    return operation(number)
+  }
+}
+function zero(operation) {
+  return expression(0, operation)   
+}
+function one(operation) {
+  return expression(1, operation)
+}
+function two(operation) {
+  return expression(2, operation)
+}
+function three(operation) {
+  return expression(3, operation)
+}
+function four(operation) {
+  return expression(4, operation)
+}
+function five(operation) {
+  return expression(5, operation)
+}
+function six(operation) {
+  return expression(6, operation)
+}
+function seven(operation) {
+  return expression(7, operation)
+}
+function eight(operation) {
+  return expression(8, operation)
+}
+function nine(operation) {
+  return expression(9, operation)
+}
+
+function plus(a) {
+  return function(b){
+    return b + a
+  }
+}
+function minus(a) {
+  return function(b){
+    return b - a
+  }
+}
+function times(a) {
+  return function(b){
+    return b * a
+  }
+}
+function dividedBy(a) {
+  return function(b){
+    return b / a
+  }
+}
+
+seven(times(five()))
